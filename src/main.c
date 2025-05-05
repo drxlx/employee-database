d5fd7d033cff24a67485c49612af82129839cc41
Anton Dryakhlykh <drxlx@ya.ru>
2025-05-05 11:39:56 +0300

    Adds working with command line arguments

--- src/main.c
+++ src/main.c
@@ -8,8 +8,41 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+    printf("Usage – %s -n -f <database file>\n", argv[0]);
+    printf("\t -n – create database file\n");
+    printf("\t -f – (required) path to database file\n");
+    return;
 }
 
-int main(int argc, char *argv[]) { 
-	
+int main(int argc, char *argv[]) {
+	int c;
+    bool newfile = false;
+    char *filepath = NULL;
+
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option – %c\n", c);
+                break;
+            default:
+                return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+        return 0;
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("Filepath: %s\n", filepath);
+
+    return 0;
 }
